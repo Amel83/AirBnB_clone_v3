427b5f94361721e2b67448028df1a208136081a2
amel83 <tesfaneshk222@gmail.com>
2023-10-29 21:00:36 +0000

    8!2

---
+++ api/v1/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+'''is this thing so tedious hmmm perfect choice for me though
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    '''
+    why is this thing so tedious hmmm perfect choice for me though
+    '''
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    why is this thing so tedious hmmm perfect choice for me though
+    '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+'''create a route  on the object app_views.
+'''
+
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    '''
+    still trying this thing for the whole day no idea why
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    still trying this thing for the whole day no idea why
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
